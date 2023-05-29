fd48b44da7017a74fbcd5a030c7b4d07c3739e04
KyrneDev <charlie.kern01@gmail.com>
2023-05-28 22:02:54 -0700

    Remove var

--- src/workers/userDataWorker.ts
+++ src/workers/userDataWorker.ts
@@ -87,8 +87,6 @@ const getStakedBalance = async (
   await Promise.all([stakesPromise, farmsPromise]);
 };
 
-let gaugePromise;
-
 /**
  * Helper method - Updates portfolio related data
  *  - userWalletAddress: Address of user in the blockchain
